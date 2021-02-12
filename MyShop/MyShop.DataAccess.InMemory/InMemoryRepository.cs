e69ddd5962c892deff09823dbd118904a8f1d76b
rishabhparaskar <rishabh.paraskar@gmail.com>
2021-02-12 18:06:17 +0530

    implemented Generic respository

---
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,70 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class InMemoryRepository<T> where T : BaseEntity 
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;
+        string className;
+        public InMemoryRepository()
+       {
+            className = typeof(T).Name;
+            items = cache[className] as List<T>;
+            if (items == null) {
+                items = new List<T>();
+            }
+       }
+
+        public void commit() {
+            cache[className] = items;
+        }
+        public void insert(T t) {
+            items.Add(t);
+        }
+
+        public void update(T t) {
+           
+            T tToUpdate =items.Find(i =>i.Id == t.Id);
+            if (tToUpdate != null)
+            {
+                tToUpdate = t;
+            }
+            else {
+                throw new Exception(className+" Not Found");
+            }
+        }
+        public T find(string Id) {
+          
+            T toFind= items.Find(i => i.Id == Id);
+            if (toFind != null)
+            {
+                return toFind;
+            }
+            else {
+                throw new Exception(className + " Not Found");
+            }
+        }
+        public void delete(string Id) {
+            T tToUpdate = items.Find(i => i.Id == Id);
+            if (tToUpdate != null)
+            {
+                items.Remove(tToUpdate);
+            }
+            else
+            {
+                throw new Exception(className + " Not Found");
+            }
+        }
+
+        public IQueryable<T> collection() {
+            return items.AsQueryable();
+        }
+    }
+}
---
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.DataAccess.InMemory;
+using MyShop.Core.Models;
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        InMemoryRepository<ProductCategory> context;
+
+        public ProductCategoryManagerController() {
+            context = new InMemoryRepository<ProductCategory>();
+        }
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List <ProductCategory> productCategory = context.collection().ToList();
+            return View(productCategory);
+        }
+
+        public ActionResult Create() {
+            ProductCategory productCategory = new ProductCategory(); 
+            return View(productCategory);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productCategory);
+            }
+            else {
+                context.insert(productCategory);
+                context.commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id) {
+            ProductCategory productCategory = context.find(Id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+
+            }
+            else {
+                return View(productCategory);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productCategory, string Id) {
+            ProductCategory productCategoryTOEdit = context.find(Id);
+            if (productCategoryTOEdit == null)
+            {
+                return HttpNotFound();
+
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(productCategory);
+                }
+                else {
+                    productCategoryTOEdit.Category = productCategory.Category;
+                    context.commit();
+                    return RedirectToAction("Index");
+                }
+            }
+
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            ProductCategory productCategoryToDelete = context.find(Id);
+            if (productCategoryToDelete == null)
+            {
+                return HttpNotFound();
+
+            }
+            else
+            {
+                return View(productCategoryToDelete);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            ProductCategory productCategoryToDelete = context.find(Id);
+            if (productCategoryToDelete == null)
+            {
+                return HttpNotFound();
+
+            }
+            else
+            {
+                context.delete(Id);
+                context.commit();
+                return RedirectToAction("index");
+            }
+        }
+    }
+}
